736767a216b84896925460b442b95d1a33d48e30
prokash <pprokash102@gmail.com>
2022-05-23 01:50:20 +0600

    user data get api create and update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Pro Electric Tools Manufacturer Information :
 
 > - Live Site Link : https://electric-tools-manufacturer.web.app
+> - server : https://electric-tools-server.herokuapp.com/
 
 ## Technology Used ( Packages Install )
 
--- index.js
+++ index.js
@@ -9,6 +9,48 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongodb
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l6pul.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    const toolsCollection = client.db("toolsManager").collection("tools");
+    const userCollection = client.db("toolsManager").collection("users");
+
+    //get tools api
+    app.get("/tools", async (req, res) => {
+      const tools = toolsCollection.find({});
+      const result = await tools.toArray();
+      res.send(result);
+    });
+    //user api create put
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+    });
+  } finally {
+  }
+};
+run().catch(console.dir);
+
 //global get api
 app.get("/", (req, res) => {
   res.send("Welcome To Server");
